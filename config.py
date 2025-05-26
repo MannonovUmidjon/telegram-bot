4ebb25068ca9e5ac65e3efa49e797baed5c8a586
MannonovUmidjon <umidjonmannonov39@gmail.com>
2025-05-27 02:13:50 +0500

    config.py

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 
-TOKEN = "7348821242:AAFIlAoPQrGkpZM4phNlDoflIzcrHrH0g4E"
+TOKEN = "7348821242:AAEYxu9eXAJjV9mbovPkYO5oaLQBMeRNmwo"
 ADMINS = [7646506662]
 LANGUAGES = ["uz", "ru", "en", "tr", "de", "fr", "it", "es", "ar", "zh"]
 DEFAULT_LANGUAGE = "uz"
